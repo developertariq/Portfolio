65b5f3fe4c133f62707d9b597416eba9a01f688e
developertariq <dev.tariqul.islam@gmail.com>
2022-11-15 18:12:00 +0600

    Add code to open and close menu

---
+++ index.js
@@ -0,0 +1,9 @@
+const mobileMenu = document.getElementById("dropdown");
+
+function openNav() {
+  mobileMenu.style.display= 'block';
+}
+
+function closeNav() {
+  mobileMenu.style.display= 'none';
+}
