193d3d04f10dbb20233faea61fb7a2ff05bec08b
developertariq <dev.tariqul.islam@gmail.com>
2022-11-17 20:42:10 +0600

    Refactor for dynamically load html

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const mobileMenu = document.getElementById("dropdown");
 function openNav() {
   mobileMenu.style.display= 'block';
   mobileMenu.style.position = 'fixed';
-  document.getElementsByTagName('body').style.overflow = 'hidden';
 }
 
 function closeNav() {
@@ -12,38 +11,27 @@ function closeNav() {
 }
 
 function showDetails(container) {
-  const popupWindow = document.querySelector("#popup-window");
-  popupWindow.classList.add('show-details');
-
-  popupWindow.innerHTML = "<div class='heading-close margin-10'>\
-  <h3 id='show-details-name' class='work-name popup-name'></h3>\
-  <span class='closebtn' onclick='closeDetails()'>&times;</span></div>\
-  <ul id='popup-list' class='technologies'></ul>\
-  <div class='popup-image-container'><img id='popup-image' src='' alt='' /></div>\
-  <p class='pop-up-description'></p>\
-  <div class='live-btn-container'>\
-  <a id='see-live' href='#' target='_blank'>See Live <i class='fas fa-external-link-alt'></i></a>\
-  <a id='see-source' href='#' target='_blank'>See Source <i class='fab fa-github'></i></a></div>";
-
-  popupWindow.classList.toggle('hide');
+  let popupWindow = document.getElementById('popup-window') 
   fillPopupWindow (popupWindow, container.id);
-  overlay.classList.toggle('hide');
+  popupWindow.classList.add('show-details');
+  popupWindow.classList.toggle('hide');
 }
 
 function closeDetails() {
   document.getElementById("popup-window").classList.remove('show-details');
   document.getElementById("popup-window").classList.toggle('hide');
-  overlay.classList.toggle('hide');
 }
 
 function fillPopupWindow (targetWindow, workId) {
   const work = works.find(a => a.id === workId);
+  
   targetWindow.querySelector("h3").innerText = work.name;
   targetWindow.querySelector("p").innerText = work.description;
   targetWindow.querySelector("#see-live").href = work.livelink;
   targetWindow.querySelector("#see-source").href = work.sourcelink;
   targetWindow.querySelector("#popup-image").src = work.image;
   targetWindow.querySelector("#popup-image").alt = work.name;
+  
   let list = targetWindow.querySelector("#popup-list");
   list.innerHTML = "";
   for (let i = 0; i < work.technology.length; i++) {
@@ -52,12 +40,17 @@ function fillPopupWindow (targetWindow, workId) {
     item.classList.add("technology");
     list.appendChild(item);
   }
+  console.log(targetWindow);
 }
 
 function fillWindow (targetWindow) {
   const work = works.find(a => a.id === targetWindow.id);
   targetWindow.querySelector("h3").innerText = work.name;
   targetWindow.querySelector("p").innerText = work.description;
+  const a = targetWindow.querySelector('a');
+  if (targetWindow.id !== 'work2'){
+    a.classList.add('hide');
+  }
   let list = targetWindow.querySelector(".proj-lang");
   if  (list !== null) {
     list.innerHTML = "";
@@ -89,6 +82,10 @@ function fillWindow2 (targetWindow) {
     }
   }
 }
+function loadOnPageLoad () {
+  createPopupWindow ();
+  loadRecentWorks ()
+}
 
 function loadRecentWorks () {
   for(let i = 0; i < works.length; i++) {
@@ -100,9 +97,87 @@ function loadRecentWorks () {
     }
   }
 }
+
+function createPopupWindow () {
+  const h3 = document.createElement('h3');
+  h3.id = 'show-details-name';
+  h3.classList.add('work-name','popup-name');
+  const span = document.createElement('button');
+  span.classList.add('closebtn');
+  const clsbtn = document.createElement('i');
+  clsbtn.classList.add('fas','fa-times');
+  span.setAttribute('onclick', 'closeDetails()')
+  span.appendChild(clsbtn);
+  const namediv = document.createElement('div');
+  namediv.classList.add('heading-close', 'margin-10');
+  namediv.appendChild(h3);
+  namediv.appendChild(span);
   
+  const ul = document.createElement('ul');
+  ul.id = 'popup-list';
+  ul.classList.add('technologies');
+  
+  const img = document.createElement('img');
+  img.id = 'popup-image';
+  img.setAttribute('src','');
+  img.setAttribute('alt','');
+  const imgdiv = document.createElement('div');
+  imgdiv.classList.add('popup-image-container');
+  imgdiv.appendChild(img);
+  
+  const para = document.createElement('p');
+  para.classList.add('pop-up-description');
+
+  const seelive = document.createElement('a');
+  seelive.id = 'see-live';
+  seelive.setAttribute('href','#');
+  seelive.setAttribute('target','_blank');
+  seelive.innerText = 'See Live ';
+
+  const externalicon = document.createElement('i');
+  externalicon.classList.add('fas','fa-external-link-alt');
+  seelive.appendChild(externalicon);
+
+  const seesource = document.createElement('a');
+  seesource.id = 'see-source';
+  seesource.setAttribute('href','#');
+  seesource.setAttribute('target','_blank');
+  seesource.innerText = 'See Live ';
+
+  const githubicon = document.createElement('i');
+  githubicon.classList.add('fab','fa-github');
+  seesource.appendChild(githubicon);
+  
+  const btndiv = document.createElement('div');
+  btndiv.classList.add('live-btn-container');
+  btndiv.appendChild(seelive);
+  btndiv.appendChild(seesource);
+
+  const popupWindow = document.createElement('div');
+  popupWindow.id = 'popup-window';
+  popupWindow.classList.add('hide');
+  popupWindow.appendChild(namediv);
+  popupWindow.appendChild(ul);
+  popupWindow.appendChild(imgdiv);
+  popupWindow.appendChild(para);
+  popupWindow.appendChild(btndiv);
+  
+  document.querySelector('body').appendChild(popupWindow);
+}
+
 const overlay = document.createElement('div');
-overlay.classList.add('popupOverlay');
-overlay.classList.add('hide');
-document.querySelector('body').appendChild(overlay);
-overlay.appendChild(document.querySelector('#popup-window'))
+function lockBack (){
+  overlay.classList.add('popupOverlay');
+  overlay.classList.add('hide');
+  document.querySelector('body').appendChild(overlay);
+  overlay.appendChild(document.querySelector('#popup-window'));
+}
+
+
+
+
+
+
+
+
+
