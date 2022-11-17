64bc05f2adaad721804e16a7e71d99c8ce259fef
developertariq <dev.tariqul.islam@gmail.com>
2022-11-17 23:42:25 +0600

    Validate user input at client side

--- validation.js
+++ validation.js
@@ -1,6 +1,6 @@
 
 function showMessage(input, message, type) {
-	const msg = input.parentElement.querySelector("small");
+	const msg = document.querySelector("small");
 	msg.innerText = message;
 	input.className = type ? "success" : "error";
 	return type;
@@ -36,16 +36,44 @@ function validateEmail(input, requiredMsg, invalidMsg) {
 }
 
 const form = document.querySelector("#contact-me-form");
-
-const EMAIL_REQUIRED = "Please enter your email";
-const EMAIL_INVALID = "Please enter a correct email address format";
+const NAME_REQUIRED = "Please enter your full name.";
+const f_NAME_REQUIRED = "Please enter your first name.";
+const l_NAME_REQUIRED = "Please enter your last name.";
+const EMAIL_REQUIRED = "Please enter your email address.";
+const EMAIL_INVALID = "Please enter a correct email address format.";
 
 form.addEventListener("submit", function (event) {
 	event.preventDefault();
-	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
+  let fullNameValid;
+  let firstNameValid;
+  let lastNameValid;
+  let emailValid;
+  if (window.matchMedia("(max-width: 700px)").matches) {
+    fullNameValid = hasValue(form.elements["name"], NAME_REQUIRED);
+    if(!fullNameValid) return;
+  } else {
+    firstNameValid = hasValue(form.elements["firstname"], f_NAME_REQUIRED);
+    if(!firstNameValid) return;
+    lastNameValid = hasValue(form.elements["lastname"], l_NAME_REQUIRED);
+    if(!lastNameValid) return;
+  }
 
-	if (emailValid) {
-    
-	}
+  emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
+  if (window.matchMedia("(max-width: 700px)").matches) {
+    if (emailValid && fullNameValid) {
+      form.submit();
+    }
+  } else {
+    if (emailValid && firstNameValid && lastNameValid) {
+      form.submit();
+    }
+  } 
+  clearField()
 });
 
+function clearField() {
+  form.elements["firstname"].value='';
+  form.elements["lastname"].value='';
+  form.elements["name"].value='';
+  form.elements["email"].value='';
+}
