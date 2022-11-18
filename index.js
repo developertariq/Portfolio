d27748fd956b41ed8020166348257ca1f6dad987
developertariq <dev.tariqul.islam@gmail.com>
2022-11-18 08:45:57 +0600

    Update file to ensure eslint

--- index.js
+++ index.js
@@ -1,150 +1,150 @@
 
-const mobileMenu = document.getElementById("dropdown");
+const mobileMenu = document.getElementById('dropdown');
 
 function openNav() {
-  mobileMenu.style.display= 'block';
+  mobileMenu.style.display = 'block';
   mobileMenu.style.position = 'fixed';
 }
 
 function closeNav() {
-  mobileMenu.style.display= 'none';
+  mobileMenu.style.display = 'none';
 }
 
 function showDetails(container) {
-  let popupWindow = document.getElementById('popup-window') 
-  fillPopupWindow (popupWindow, container.id);
+  const popupWindow = document.getElementById('popup-window');
+  fillPopupWindow(popupWindow, container.id);
   popupWindow.classList.add('show-details');
   popupWindow.classList.toggle('hide');
 }
 
 function closeDetails() {
-  document.getElementById("popup-window").classList.remove('show-details');
-  document.getElementById("popup-window").classList.toggle('hide');
+  document.getElementById('popup-window').classList.remove('show-details');
+  document.getElementById('popup-window').classList.toggle('hide');
 }
 
-function fillPopupWindow (targetWindow, workId) {
-  const work = works.find(a => a.id === workId);
-  
-  targetWindow.querySelector("h3").innerText = work.name;
-  targetWindow.querySelector("p").innerText = work.description;
-  targetWindow.querySelector("#see-live").href = work.livelink;
-  targetWindow.querySelector("#see-source").href = work.sourcelink;
-  targetWindow.querySelector("#popup-image").src = work.image;
-  targetWindow.querySelector("#popup-image").alt = work.name;
-  
-  let list = targetWindow.querySelector("#popup-list");
-  list.innerHTML = "";
+function fillPopupWindow(targetWindow, workId) {
+  const work = works.find((a) => a.id === workId);
+
+  targetWindow.querySelector('h3').innerText = work.name;
+  targetWindow.querySelector('p').innerText = work.description;
+  targetWindow.querySelector('#see-live').href = work.livelink;
+  targetWindow.querySelector('#see-source').href = work.sourcelink;
+  targetWindow.querySelector('#popup-image').src = work.image;
+  targetWindow.querySelector('#popup-image').alt = work.name;
+
+  const list = targetWindow.querySelector('#popup-list');
+  list.innerHTML = '';
   for (let i = 0; i < work.technology.length; i++) {
-    var item = document.createElement("li");
+    const item = document.createElement('li');
     item.appendChild(document.createTextNode(work.technology[i]));
-    item.classList.add("technology");
+    item.classList.add('technology');
     list.appendChild(item);
   }
   console.log(targetWindow);
 }
 
-function fillWindow (targetWindow) {
-  const work = works.find(a => a.id === targetWindow.id);
-  targetWindow.querySelector("h3").innerText = work.name;
-  targetWindow.querySelector("p").innerText = work.description;
-  
-  let list = targetWindow.querySelector(".proj-lang");
-  if  (list !== null) {
-    list.innerHTML = "";
+function fillWindow(targetWindow) {
+  const work = works.find((a) => a.id === targetWindow.id);
+  targetWindow.querySelector('h3').innerText = work.name;
+  targetWindow.querySelector('p').innerText = work.description;
+
+  const list = targetWindow.querySelector('.proj-lang');
+  if (list !== null) {
+    list.innerHTML = '';
     for (let i = 0; i < work.technology.length; i++) {
-      var item = document.createElement("li");
+      const item = document.createElement('li');
       item.appendChild(document.createTextNode(work.technology[i]));
-      item.classList.add("technology");
+      item.classList.add('technology');
       list.appendChild(item);
     }
   }
-  targetWindow.style.background = "linear-gradient(179.35deg, rgba(38, 38, 38, 0) 0.85%, rgba(38, 38, 38, 0.9) 84%), url(" + work.image+")";
+  targetWindow.style.background = `linear-gradient(179.35deg, rgba(38, 38, 38, 0) 0.85%, rgba(38, 38, 38, 0.9) 84%), url(${work.image})`;
 }
 
-function fillWindow2 (targetWindow) {
-  const work = works.find(a => a.id === targetWindow.id);
-  targetWindow.querySelector("h3").innerText = work.name;
-  targetWindow.querySelector("p").innerText = work.description;
-  targetWindow.querySelector("#img-animation").src = work.image;
-  targetWindow.querySelector("#img-animation").alt = work.name;
-  let list = targetWindow.querySelector("#top-proj-lang");
-  if  (list !== null) {
-    list.innerHTML = "";
-  
+function fillWindow2(targetWindow) {
+  const work = works.find((a) => a.id === targetWindow.id);
+  targetWindow.querySelector('h3').innerText = work.name;
+  targetWindow.querySelector('p').innerText = work.description;
+  targetWindow.querySelector('#img-animation').src = work.image;
+  targetWindow.querySelector('#img-animation').alt = work.name;
+  const list = targetWindow.querySelector('#top-proj-lang');
+  if (list !== null) {
+    list.innerHTML = '';
+
     for (let i = 0; i < work.technology.length; i++) {
-      var item = document.createElement("li");
+      const item = document.createElement('li');
       item.appendChild(document.createTextNode(work.technology[i]));
-      item.classList.add("technology");
+      item.classList.add('technology');
       list.appendChild(item);
     }
   }
 }
-function loadOnPageLoad () {
-  createPopupWindow ();
-  loadRecentWorks ()
-}
 
-function loadRecentWorks () {
-  for(let i = 0; i < works.length; i++) {
+window.addEventListener('load', () => {
+  createPopupWindow();
+  loadRecentWorks();
+});
+
+function loadRecentWorks() {
+  for (let i = 0; i < works.length; i++) {
     if (i === 0) {
-      document.getElementById(works[i].id).addEventListener("load", fillWindow2(document.getElementById(works[i].id)));
-    }
-    else {
-      document.getElementById(works[i].id).addEventListener("load", fillWindow(document.getElementById(works[i].id)));
+      document.getElementById(works[i].id).addEventListener('load', fillWindow2(document.getElementById(works[i].id)));
+    } else {
+      document.getElementById(works[i].id).addEventListener('load', fillWindow(document.getElementById(works[i].id)));
     }
   }
 }
 
-function createPopupWindow () {
+function createPopupWindow() {
   const h3 = document.createElement('h3');
   h3.id = 'show-details-name';
-  h3.classList.add('work-name','popup-name');
+  h3.classList.add('work-name', 'popup-name');
   const span = document.createElement('button');
   span.classList.add('closebtn');
   const clsbtn = document.createElement('i');
-  clsbtn.classList.add('fas','fa-times');
-  span.setAttribute('onclick', 'closeDetails()')
+  clsbtn.classList.add('fas', 'fa-times');
+  span.setAttribute('onclick', 'closeDetails()');
   span.appendChild(clsbtn);
   const namediv = document.createElement('div');
   namediv.classList.add('heading-close', 'margin-10');
   namediv.appendChild(h3);
   namediv.appendChild(span);
-  
+
   const ul = document.createElement('ul');
   ul.id = 'popup-list';
   ul.classList.add('technologies');
-  
+
   const img = document.createElement('img');
   img.id = 'popup-image';
-  img.setAttribute('src','');
-  img.setAttribute('alt','');
+  img.setAttribute('src', '');
+  img.setAttribute('alt', '');
   const imgdiv = document.createElement('div');
   imgdiv.classList.add('popup-image-container');
   imgdiv.appendChild(img);
-  
+
   const para = document.createElement('p');
   para.classList.add('pop-up-description');
 
   const seelive = document.createElement('a');
   seelive.id = 'see-live';
-  seelive.setAttribute('href','#');
-  seelive.setAttribute('target','_blank');
+  seelive.setAttribute('href', '#');
+  seelive.setAttribute('target', '_blank');
   seelive.innerText = 'See Live ';
 
   const externalicon = document.createElement('i');
-  externalicon.classList.add('fas','fa-external-link-alt');
+  externalicon.classList.add('fas', 'fa-external-link-alt');
   seelive.appendChild(externalicon);
 
   const seesource = document.createElement('a');
   seesource.id = 'see-source';
-  seesource.setAttribute('href','#');
-  seesource.setAttribute('target','_blank');
+  seesource.setAttribute('href', '#');
+  seesource.setAttribute('target', '_blank');
   seesource.innerText = 'See Live ';
 
   const githubicon = document.createElement('i');
-  githubicon.classList.add('fab','fa-github');
+  githubicon.classList.add('fab', 'fa-github');
   seesource.appendChild(githubicon);
-  
+
   const btndiv = document.createElement('div');
   btndiv.classList.add('live-btn-container');
   btndiv.appendChild(seelive);
@@ -158,23 +158,14 @@ function createPopupWindow () {
   popupWindow.appendChild(imgdiv);
   popupWindow.appendChild(para);
   popupWindow.appendChild(btndiv);
-  
+
   document.querySelector('body').appendChild(popupWindow);
 }
 
 const overlay = document.createElement('div');
-function lockBack (){
+function lockBack() {
   overlay.classList.add('popupOverlay');
   overlay.classList.add('hide');
   document.querySelector('body').appendChild(overlay);
   overlay.appendChild(document.querySelector('#popup-window'));
 }
-
-
-
-
-
-
-
-
-
--- validation.js
+++ validation.js
@@ -1,79 +1,77 @@
-
 function showMessage(input, message, type) {
-	const msg = document.querySelector("small");
-	msg.innerText = message;
-	input.className = type ? "success" : "error";
-	return type;
+  const msg = document.querySelector('small');
+  msg.innerText = message;
+  input.className = type ? 'success' : 'error';
+  return type;
 }
 
 function showError(input, message) {
-	return showMessage(input, message, false);
+  return showMessage(input, message, false);
 }
 
 function showSuccess(input) {
-	return showMessage(input, "", true);
+  return showMessage(input, '', true);
 }
 
 function hasValue(input, message) {
-	if (input.value.trim() === "") {
-		return showError(input, message);
-	}
-	return showSuccess(input);
+  if (input.value.trim() === '') {
+    return showError(input, message);
+  }
+  return showSuccess(input);
 }
 
 function validateEmail(input, requiredMsg, invalidMsg) {
-	if (!hasValue(input, requiredMsg)) {
-		return false;
-	}
+  if (!hasValue(input, requiredMsg)) {
+    return false;
+  }
 
-	const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/;
+  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/;
 
-	const email = input.value.trim();
-	if (!emailRegex.test(email)) {
-		return showError(input, invalidMsg);
-	}
-	return true;
+  const email = input.value.trim();
+  if (!emailRegex.test(email)) {
+    return showError(input, invalidMsg);
+  }
+  return true;
 }
 
-const form = document.querySelector("#contact-me-form");
-const NAME_REQUIRED = "Please enter your full name.";
-const f_NAME_REQUIRED = "Please enter your first name.";
-const l_NAME_REQUIRED = "Please enter your last name.";
-const EMAIL_REQUIRED = "Please enter your email address.";
-const EMAIL_INVALID = "Please enter a correct email address format.";
+const form = document.querySelector('#contact-me-form');
+const FULLNAME_REQUIRED = 'Please enter your full name.';
+const FIRSTNAME_REQUIRED = 'Please enter your first name.';
+const LASTNAME_REQUIRED = 'Please enter your last name.';
+const EMAIL_REQUIRED = 'Please enter your email address.';
+const EMAIL_INVALID = 'Please enter a correct email address format.';
 
-form.addEventListener("submit", function (event) {
-	event.preventDefault();
+function clearField() {
+  form.elements.firstname.value = '';
+  form.elements.lastname.value = '';
+  form.elements.name.value = '';
+  form.elements.email.value = '';
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
   let fullNameValid;
   let firstNameValid;
   let lastNameValid;
-  let emailValid;
-  if (window.matchMedia("(max-width: 700px)").matches) {
-    fullNameValid = hasValue(form.elements["name"], NAME_REQUIRED);
-    if(!fullNameValid) return;
+
+  if (window.matchMedia('(max-width: 700px)').matches) {
+    fullNameValid = hasValue(form.elements.name, FULLNAME_REQUIRED);
+    if (!fullNameValid) return;
   } else {
-    firstNameValid = hasValue(form.elements["firstname"], f_NAME_REQUIRED);
-    if(!firstNameValid) return;
-    lastNameValid = hasValue(form.elements["lastname"], l_NAME_REQUIRED);
-    if(!lastNameValid) return;
+    firstNameValid = hasValue(form.elements.firstname, FIRSTNAME_REQUIRED);
+    if (!firstNameValid) return;
+    lastNameValid = hasValue(form.elements.lastname, LASTNAME_REQUIRED);
+    if (!lastNameValid) return;
   }
 
-  emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
-  if (window.matchMedia("(max-width: 700px)").matches) {
+  const emailValid = validateEmail(form.elements.email, EMAIL_REQUIRED, EMAIL_INVALID);
+  if (window.matchMedia('(max-width: 700px)').matches) {
     if (emailValid && fullNameValid) {
       form.submit();
+      clearField();
     }
-  } else {
-    if (emailValid && firstNameValid && lastNameValid) {
-      form.submit();
-    }
-  } 
-  clearField()
+  } else if (emailValid && firstNameValid && lastNameValid) {
+    form.submit();
+    clearField();
+  }
 });
-
-function clearField() {
-  form.elements["firstname"].value='';
-  form.elements["lastname"].value='';
-  form.elements["name"].value='';
-  form.elements["email"].value='';
-}
